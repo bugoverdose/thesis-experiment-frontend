48aff0eb5a229eade9091626658dcd2cbe8e750b
Jinwoo Jeong <bugod96@gmail.com>
2021-05-27 12:24:45 +0900

    oauth route fix

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,5 +1,5 @@
 export const oauthRoute = {
-  kakaoLogin: `https:/kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`, // callback UrL에 code값 받아오기.
+  kakaoLogin: `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`, // callback UrL에 code값 받아오기.
   kakaoRedirectURI: "/oauth",
 };
 
